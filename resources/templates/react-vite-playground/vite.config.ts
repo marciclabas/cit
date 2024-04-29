2e99617edc83ef2370e71fd05736c9891e35bad4
m4rs <marcel@marcelclaramunt.cat>
2024-04-29 09:57:02 +0200

    euh

--- resources/templates/react-vite-playground/vite.config.ts
+++ resources/templates/react-vite-playground/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 // import { visualizer } from 'rollup-plugin-visualizer'
 // import mkcert from 'vite-plugin-mkcert'
 // import viteCompression from 'vite-plugin-compression';
-// import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
